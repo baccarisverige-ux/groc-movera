91ed9c3ec7aa7f295157a15a3805b2d0a6615a25
baccarisverige-ux <baccari.sverige@gmail.com>
2026-08-29 06:46:38 +0200

    Point GitHub Pages deploy at groc-movera

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Movera-host1/',
+  base: '/groc-movera/',
   plugins: [react()],
   build: {
     assetsInlineLimit: 0,
