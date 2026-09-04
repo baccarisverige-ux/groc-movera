7b3488d352e9de4dcb09b5f3126be5b4b50dac87
baccarisverige-ux <baccari.sverige@gmail.com>
2026-09-04 21:41:36 +0200

    Separate offer item motion from retired sheet runtime

---
+++ src/features/map/mapOfferItemMotion.config.js
@@ -0,0 +1,13 @@
+export const MAP_OFFER_ITEM_MOTION = Object.freeze({
+  activeScale: 1,
+  enterScale: 0.992,
+  enterY: 8,
+  exitScale: 0.992,
+  exitY: -4,
+  inactiveScale: 1,
+  initialOpacity: 0.9,
+  layout: false,
+  stagger: 0.015,
+  tapScale: 0.995,
+  spring: Object.freeze({ stiffness: 390, damping: 34, mass: 0.72 }),
+})
